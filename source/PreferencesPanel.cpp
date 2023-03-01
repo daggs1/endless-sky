83773235a2eed028017d7df757f2d3b8ca6bd5c7
Dagg Stompler <daggs@gmx.com>
2023-03-01 20:35:09 +0200

    Preferences: add unvisited shipyards entry
    add a preference entry for hide unvisited shipyards which will be displayed
    in the preference menu
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/PreferencesPanel.cpp
+++ source/PreferencesPanel.cpp
@@ -83,6 +83,7 @@ namespace {
 	const string HIDE_OUTFITTERS = "Hide unvisited outfitters";
 	const string HIDE_OUTFITS = "Hide unknown outfits";
 	const string HIDE_SHIP_MODELS = "Hide unknown ship models";
+	const string HIDE_SHIPYARDS = "Hide unvisited shipyards";
 
 	// How many pages of controls and settings there are.
 	const int CONTROLS_PAGE_COUNT = 2;
@@ -701,7 +702,8 @@ void PreferencesPanel::DrawSettings()
 		ALERT_INDICATOR,
 		HIDE_OUTFITTERS,
 		HIDE_OUTFITS,
-		HIDE_SHIP_MODELS
+		HIDE_SHIP_MODELS,
+		HIDE_SHIPYARDS
 	};
 
 	bool isCategory = true;
