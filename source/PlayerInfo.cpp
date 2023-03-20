d3e5c7b201b002edeb31effa2c5a72ddfd4047a1
Dagg Stompler <daggs@gmx.com>
2023-03-20 21:47:05 +0200

    PlayerInfo: replace visitedPlanets's value with struct
    in then next commit, visitedPlanets will hold data on the planet's
    shipyard in addition to the outfitter, so adjust the data structure
    to support it.
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2462,7 +2462,9 @@ void PlayerInfo::Visit(const System &system)
 // Mark the given planet as visited.
 void PlayerInfo::Visit(const Planet &planet)
 {
-	visitedPlanets.insert(make_pair(&planet, false));
+	struct PlanetVisitedEstablishmets data = { 0 };
+
+	visitedPlanets.insert(make_pair(&planet, data));
 }
 
 
@@ -2949,7 +2951,7 @@ set<string> &PlayerInfo::Collapsed(const string &name)
 // Mark outfitter at planet as visited
 void PlayerInfo::VisitOutfitterAt(const Planet &planet)
 {
-	visitedPlanets[&planet] = true;
+	visitedPlanets[&planet].outfitter = true;
 }
 
 
@@ -2957,7 +2959,7 @@ void PlayerInfo::VisitOutfitterAt(const Planet &planet)
 // Check if the outfitter at planet was visited
 bool PlayerInfo::OutfitterVisitedAt(const Planet &planet)
 {
-	return (Preferences::Has(HIDE_OUTFITTERS)) ? visitedPlanets[&planet] : true;
+	return (Preferences::Has(HIDE_OUTFITTERS)) ? visitedPlanets[&planet].outfitter : true;
 }
 
 
@@ -4514,7 +4516,7 @@ void PlayerInfo::Save(DataWriter &out) const
 		});
 
 	// Save a list of planets the player has visited.
-	using PlanetEntry = pair<const Planet *const, bool>;
+	using PlanetEntry = pair<const Planet *const, struct PlanetVisitedEstablishmets>;
 	WriteSorted(visitedPlanets,
 		[](const PlanetEntry *lhs, const PlanetEntry *rhs)
 			{ return lhs->first->TrueName() < rhs->first->TrueName(); },
@@ -4522,7 +4524,7 @@ void PlayerInfo::Save(DataWriter &out) const
 		{
 			string visitedOutfitterStr = "";
 
-			if(entry.second)
+			if(entry.second.outfitter)
 				visitedOutfitterStr = "outfitter";
 
 			if(visitedOutfitterStr.empty())
