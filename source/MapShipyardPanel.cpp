35f1ceb36f94b8fa4da96222ca55dc8b37d97b49
Dagg Stompler <daggs@gmx.com>
2023-03-21 20:23:15 +0200

    maps: fix header print bug in DrawItems
    reset headerDrawed var on each category loop
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/MapShipyardPanel.cpp
+++ source/MapShipyardPanel.cpp
@@ -195,7 +195,7 @@ int MapShipyardPanel::FindItem(const string &text) const
 
 void MapShipyardPanel::DrawItems()
 {
-	bool headerDrawed = false;
+	bool headerDrawed;
 
 	if(GetUI()->IsTop(this) && player.GetPlanet() && player.GetDate() >= player.StartData().GetDate() + 12)
 		DoHelp("map advanced shops");
@@ -204,6 +204,8 @@ void MapShipyardPanel::DrawItems()
 	for(const auto &cat : categories)
 	{
 		const string &category = cat.Name();
+		headerDrawed = false;
+
 		auto it = catalog.find(category);
 		if(it == catalog.end())
 			continue;
