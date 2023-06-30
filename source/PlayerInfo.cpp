4c970643bd4e5ee15fa50dcb5052ad3adc913366
Dagg Stompler <daggs@gmx.com>
2023-06-30 21:13:40 +0300

    Ship: use newly added hardpoint functionality
    now that the scan is hardpoint and no outfit
    (valid weapon must be attached into a hardpoint so
    all is good), use it for secondary weapon selection
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2692,7 +2692,7 @@ void PlayerInfo::SelectNextSecondary()
 
 	// Find the next secondary weapon.
 	for( ; it != flagship->Outfits().end(); ++it)
-		if(it->first->Icon())
+		if(flagship->GetHardpointIcon(it->first))
 		{
 			selectedWeapons.clear();
 			selectedWeapons.insert(it->first);
@@ -2707,7 +2707,7 @@ void PlayerInfo::SelectNextSecondary()
 	// Reached the end of the list. Select all possible secondary weapons here.
 	it = flagship->Outfits().begin();
 	for( ; it != flagship->Outfits().end(); ++it)
-		if(it->first->Icon())
+		if(flagship->GetHardpointIcon(it->first))
 			selectedWeapons.insert(it->first);
 
 	// If we have only one weapon selected at this point, then the player
